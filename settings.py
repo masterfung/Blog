31b166ec8ea537b730282771e76a1734f67ebd13
Tsung Hung <hungmasterj@gmail.com>
2015-03-08 20:20:47 -0700

    commented out the import to see if it fixes the issue on AWS

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ import os
 # Controls the ordering and grouping of the admin menu.
 #
 # ADMIN_MENU_ORDER = (
-#     ("Content", ("pages.Page", "blog.BlogPost",
+# ("Content", ("pages.Page", "blog.BlogPost",
 #        "generic.ThreadedComment", ("Media Library", "fb_browse"),)),
 #     ("Site", ("sites.Site", "redirects.Redirect", "conf.Setting")),
 #     ("Users", ("auth.User", "auth.Group",)),
@@ -151,7 +151,7 @@ AUTHENTICATION_BACKENDS = ("mezzanine.core.auth_backends.MezzanineBackend",)
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # The numeric mode to set newly-uploaded files to. The value should be
@@ -318,21 +318,21 @@ OPTIONAL_APPS = (
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
 
-# FABRIC = {
-#     "SSH_USER": "", # SSH username for host deploying to
-#     "HOSTS": ALLOWED_HOSTS[:1], # List of hosts to deploy to (eg, first host)
-#     "DOMAINS": ALLOWED_HOSTS, # Domains for public site
-#     "REPO_URL": "ssh://hg@bitbucket.org/user/project", # Project's repo URL
-#     "VIRTUALENV_HOME":  "", # Absolute remote path for virtualenvs
-#     "PROJECT_NAME": "", # Unique identifier for project
-#     "REQUIREMENTS_PATH": "requirements.txt", # Project's pip requirements
-#     "GUNICORN_PORT": 8000, # Port gunicorn will listen on
-#     "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
-#     "DB_PASS": "", # Live database password
-#     "ADMIN_PASS": "", # Live admin user password
-#     "SECRET_KEY": SECRET_KEY,
-#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
-# }
+FABRIC = {
+    #     "SSH_USER": "", # SSH username for host deploying to
+    #     "HOSTS": ALLOWED_HOSTS[:1], # List of hosts to deploy to (eg, first host)
+    #     "DOMAINS": ALLOWED_HOSTS, # Domains for public site
+    "REPO_URL": "ssh://git@github.com:masterfung/Blog.git",  # Project's repo URL
+    #     "VIRTUALENV_HOME":  "", # Absolute remote path for virtualenvs
+    #     "PROJECT_NAME": "", # Unique identifier for project
+    #     "REQUIREMENTS_PATH": "requirements.txt", # Project's pip requirements
+    #     "GUNICORN_PORT": 8000, # Port gunicorn will listen on
+    #     "LOCALE": "en_US.UTF-8", # Should end with ".UTF-8"
+    #     "DB_PASS": "", # Live database password
+    #     "ADMIN_PASS": "", # Live admin user password
+    #     "SECRET_KEY": SECRET_KEY,
+    #     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+}
 
 
 ##################
