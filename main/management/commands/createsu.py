e89051d30b1ce21cc5e6ee57a4229a0c3630feaa
Tsung Hung <hungmasterj@gmail.com>
2015-03-08 19:18:23 -0700

    created management command for user

---
+++ main/management/commands/createsu.py
@@ -0,0 +1,11 @@
+__author__ = '@masterfung'
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "thung@me.com", "admin")
